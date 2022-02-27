c0640946b65d25fab089e35327a58d1d9de369dc
oilsakuna <oil.iop577@gmail.com>
2022-02-27 17:28:57 +0700

    first commit

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+   public void getAnimatorDance()
+   {
+       this.GetComponent<Animator>().Play("Dance");
+   }
+   public void getAnimatorWalk()
+   {
+       this.GetComponent<Animator>().Play("Walk");
+   }
+}
